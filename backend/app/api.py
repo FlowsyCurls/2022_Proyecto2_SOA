ff0fb4f3b51aed87a3850cb50ee5865cc8198d07
Shakime Sparks <jeykimers.johnson@gmail.com>
2022-10-18 05:01:16 -0600

    ADD all pods running without known connection

--- backend/app/api.py
+++ backend/app/api.py
@@ -7,20 +7,27 @@ import json
 
 app = Flask(__name__)
 
+# Set database configurations
+app.config['MYSQL_HOST'] = os.environ['RABBIT_HOST']
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DATABASE']
 
-# app.config['MYSQL_HOST'] = os.environ['RABBIT_HOST']
-# app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
-# app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
-# app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+mysql = MySQL(app)
 
-# mysql = MySQL(app)
+# to ensure that the table exist
+isCreated = False
+
+
 class Person():
     
+    # constructor
     def __init__(self, name, emotion, date):
         self.name = name
         self.emotion = emotion
         self.date = date
-        
+    
+    # to convert attributes to a dict (json)
     def to_json(self):
         return {
             "name": self.name,
@@ -28,38 +35,38 @@ class Person():
             "date": self.date
         }
 
-isCreated = False
-
 @app.route('/', methods=['GET'])
 def get_records():
 
     # Creating a connection cursor
-    # cursor = mysql.connection.cursor()
+    cursor = mysql.connection.cursor()
     
-    # Executing SQL Statements
+    # query to execute
     query_string = "SELECT * FROM emotions_table"
-    # cursor.execute(query_string)
-    
+
+    # Executing SQL Statements
+    cursor.execute(query_string)
+
     # Fetches all rows from the last executed statement
-    # result = cursor.fetchall()
+    result = cursor.fetchall()
         
     # output list
     output = []
     
     # Iterate over the rows
-    # for row in result:
-        # person = Person(row[1],row[2],row[3])
-        # output.append(person.to_json())
+    for row in result:
+        person = Person(row[1],row[2],row[3])
+        output.append(person.to_json())
         
     # we close both the cursor and connection
-    # cursor.close()
+    cursor.close()
     
     return jsonify(output)
     
 def create_record(data):
     
-    # Ensure that the table exists
     global isCreated
+    # Ensure that the table exists
     if not isCreated:
         createTable()
         isCreated = True
@@ -68,7 +75,7 @@ def create_record(data):
     data = json.loads(data)
     
     # Creating a connection cursor
-    # cursor = mysql.connection.cursor()
+    cursor = mysql.connection.cursor()
     
     # query to execute
     query_string = "INSERT INTO emotions_analysis VALUES(%s,%s,%s)"
@@ -78,27 +85,26 @@ def create_record(data):
     for person in data:
         print(person['name'], person['emotion'],person['date'])
         #Executing SQL Statements
-        # cursor.execute(query_string,person['name'],person['emotion'],person['date'])
+        cursor.execute(query_string,person['name'],person['emotion'],person['date'])
         
     #Saving the Actions performed on the DB
-    # mysql.connection.commit()
+    mysql.connection.commit()
     
     # we close both the cursor and connection
-    # cursor.close()
-
-
+    cursor.close()
+    
 def createTable():
     # Creating a connection cursor
-    # cursor = mysql.connection.cursor()
+    cursor = mysql.connection.cursor()
     
     # Query for Creating the table
     query_string = "CREATE TABLE emotions_analysis (id int NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), emotion VARCHAR(255), date VARCHAR(255))"
     
     #Executing SQL Statement
-    # cursor.execute(query_string)
+    cursor.execute(query_string)
     
     #Saving the Actions performed on the DB
-    # mysql.connection.commit()
+    mysql.connection.commit()
     
 def run():
     app.run(debug=True, host='0.0.0.0')
